af8fbf7380c3f5761206e02ee4408278fb47403f
zarkhan <hanguangzheng@qq.com>
2024-11-13 11:07:48 +0800

    init commit

--- tests/main.py
+++ tests/main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 # cython: language_level=3
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, HTTPException
 
 from fastapi_app_ext import AppExt
 
+root_path = Path(__file__).absolute().parent.parent
+
 app = FastAPI()
-AppExt(app)
+AppExt(app, root_path)
 
 
 @app.get('/')
-async def hello_world():
-    return 'Hello World!'
+async def index():
+    raise HTTPException(status_code=404,detail='Not Found')
