69ed115b6f18b09c7643ede436ac3e6f62fa442c
zarkhan <hanguangzheng@qq.com>
2024-11-25 14:11:12 +0800

    init commit

--- tests/main.py
+++ tests/main.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 # cython: language_level=3
-from pathlib import Path
-
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 
 from fastapi_app_ext import AppExt
 
-root_path = Path(__file__).absolute().parent.parent
-
 app = FastAPI()
-AppExt(app, root_path)
+AppExt(app)
 
 
 @app.get('/')
 async def index():
-    raise HTTPException(status_code=404,detail='Not Found')
+    return {'hello': 'world'}
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app)
